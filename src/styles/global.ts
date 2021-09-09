6e6dcf99519be71f7d463740510d6e1a0820ab1b
Gustavo Manca <gustavo.manca@pixter.com.br>
2021-09-08 22:13:15 -0300

    feat: add font to project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
+import { fontFaces } from './fonts'
+
 const GlobalStyles = createGlobalStyle`
+
+  ${fontFaces};
 
   * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   html {
@@ -17,9 +24,8 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-
 `
 
 export default GlobalStyles
